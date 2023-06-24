3472558702be7f4d58de1ed43b64f9dda80ef1de
Yash <yash@gmail.com>
2023-06-24 17:31:01 +0530

    Adding RestTemplate to get the Contacts

--- user_service/src/main/java/com/microservice/user_service/Controller/UserController.java
+++ user_service/src/main/java/com/microservice/user_service/Controller/UserController.java
@@ -1,11 +1,15 @@
 package com.microservice.user_service.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.microservice.user_service.Entity.Contact;
 import com.microservice.user_service.Entity.User;
 import com.microservice.user_service.Service.UserService;
 
@@ -14,11 +18,17 @@ import com.microservice.user_service.Service.UserService;
 public class UserController {
 
     @Autowired
-    UserService userService;
+    private UserService userService;
+
+    @Autowired
+    private RestTemplate restTemplate;
     
     @RequestMapping(value = "/{userID}" , method = RequestMethod.GET)
     public User getUser(@PathVariable("userID") Long userId){
-        return userService.getUser(userId);
+        User user=userService.getUser(userId);
+        List<Contact> contacts= restTemplate.getForObject("http://localhost:9002/contacts/user/"+user.getUserId(), List.class);
+        user.setContacts(contacts);
+        return user;
     }
 
 }
